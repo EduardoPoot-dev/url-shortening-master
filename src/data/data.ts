6ea954de0b1b7bea92d968016d221626b2950fe5
Bruno Moleta <brunomoleta@pm.me>
2024-06-28 17:17:41 -0300

    :recycle: refactor:Add topLinks and bottomLinks to data.ts
    The update adds two new const arrays, topLinks and bottomLinks, to the data.ts file. All data can be set at this file and imported at the components.

---
+++ src/data/data.ts
@@ -0,0 +1,11 @@
+export const topLinks = ["Features", "Pricing", "Resources"];
+export const bottomLinks = [
+  {
+    name: "Login",
+    class: "menuMobile__link",
+  },
+  {
+    name: "Sign Up",
+    class: "menuMobile__sign",
+  },
+];
